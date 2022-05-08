ceecb380fd833855de264ceb0d4a707d3f699176
VAISHNAVRAVINDRAN <104183316+VAISHNAVRAVINDRAN@users.noreply.github.com>
2022-05-08 18:31:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var mic=document.querySelector(".mic");
+var disp=document.querySelector(".display");
+mic.addEventListener("click", voice);
+function voice() {
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+recognition.onstart = function() {
+console.log("Listening...");
+};
+recognition.onspeechend = function() {
+console.log("Processing...");
+recognition.stop();
+}
+recognition.onresult = function(event) {
+var transcript = event.results[0][0].transcript;     
+var voices=transcript.toLowerCase();
+var txt=document.createElement("div");		
+txt.classList.add("client");
+txt.innerHTML=transcript;
+disp.appendChild(txt);
+if(voices=="hello" || voices=="hi" || voices=="hey") {
+var com=document.createElement("div");		
+com.classList.add("computer");
+com.innerHTML="Hi";
+disp.appendChild(com);    
+}
+if(voices=="what is your name" || voices=="name" || voices=="your name") {
+var com=document.createElement("div");		
+com.classList.add("computer");
+com.innerHTML="Myname is VimoWeb Iam your Assistant";
+disp.appendChild(com);    
+}
+};
+recognition.start();
+}
